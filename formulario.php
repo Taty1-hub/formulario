c0b0dc90a2ba63d50c89c3dc63a6fb07b4235571
Sandra <docenciasandrag@gmail.com>
2020-07-10 17:46:09 +0200

    create tabla and sav data

--- formulario.php
+++ formulario.php
@@ -12,8 +12,46 @@ License: GPLv2 or later
 Text Domain: akismet
 */
 
+// Cuando el plugin se active se crea la tabla para recoger los datos si no existe
+register_activation_hook(__FILE__, 'Kfp_Aspirante_init');
+ 
+/**
+ * Crea la tabla para recoger los datos del formulario
+ *
+ * @return void
+ */
+function Kfp_Aspirante_init() 
+{
+    global $wpdb; // Este objeto global permite acceder a la base de datos de WP
+    // Crea la tabla sólo si no existe
+    // Utiliza el mismo prefijo del resto de tablas
+    $tabla_aspirantes = $wpdb->prefix . 'aspirante';
+    // Utiliza el mismo tipo de orden de la base de datos
+    $charset_collate = $wpdb->get_charset_collate();
+    // Prepara la consulta
+    $query = "CREATE TABLE IF NOT EXISTS $tabla_aspirantes (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        nombre varchar(40) NOT NULL,
+        correo varchar(100) NOT NULL,
+        nivel_html smallint(4) NOT NULL,
+        nivel_css smallint(4) NOT NULL,
+        nivel_js smallint(4) NOT NULL,
+        aceptacion smallint(4) NOT NULL,
+        created_at datetime NOT NULL,
+        UNIQUE (id)
+        ) $charset_collate;";
+    // La función dbDelta permite crear tablas de manera segura se
+    // define en el archivo upgrade.php que se incluye a continuación
+    include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta($query); // Lanza la consulta para crear la tabla de manera segura
+}
+
+
+
 // Define el shortcode y lo asocia a una función
 add_shortcode('kfp_aspirante_form', 'Kfp_Aspirante_form');
+
+
 
 /** 
  * Define la función que ejecutará el shortcode
@@ -23,12 +61,55 @@ add_shortcode('kfp_aspirante_form', 'Kfp_Aspirante_form');
  */
 function Kfp_Aspirante_form() 
 {
-    // Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
-    // Cuando termine el formulario lo imprime con la función ob_get_clean
+    
+
+
+    
+    // Carga esta hoja de estilo para poner más bonito el formulario
+wp_enqueue_style('css_aspirante', plugins_url('formulario.css', __FILE__));
+
+global $wpdb; // Este objeto global permite acceder a la base de datos de WP
+    // Si viene del formulario  graba en la base de datos
+    // Cuidado con el último igual de la condición del if que es doble
+    if ($_POST['nombre'] != ''
+        AND is_email($_POST['correo'])
+        AND $_POST['nivel_html'] != ''
+        AND $_POST['nivel_css'] != ''
+        AND $_POST['nivel_js'] != ''      
+        AND $_POST['aceptacion'] == '1'
+    ) {
+        $tabla_aspirantes = $wpdb->prefix . 'aspirante'; 
+        $nombre = sanitize_text_field($_POST['nombre']);
+        $correo = $_POST['correo'];
+        $nivel_html = (int)$_POST['nivel_html'];
+        $nivel_css = (int)$_POST['nivel_css'];
+        $nivel_js = (int)$_POST['nivel_js'];
+        $aceptacion = (int)$_POST['aceptacion'];
+        $created_at = date('Y-m-d H:i:s');
+        $wpdb->insert(
+            $tabla_aspirantes,
+            array(
+                'nombre' => $nombre,
+                'correo' => $correo,
+                'nivel_html' => $nivel_html,
+                'nivel_css' => $nivel_css,
+                'nivel_js' => $nivel_js,
+                'aceptacion' => $aceptacion,
+                'created_at' => $created_at,
+            )
+        );
+        echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias 
+            por tu interés. En breve contactaré contigo.<p>";
+    }
+ 
+// Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
+// Cuando termine el formulario lo imprime con la función ob_get_clean
+ob_start();
+    
     ob_start();
     ?>
-    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante
-class="cuestionario"">
+    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante"
+class="cuestionario">
         <div class="form-input">
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre" required>
